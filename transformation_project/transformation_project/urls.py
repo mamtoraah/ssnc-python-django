b1eb3b9aba6968167d834df19cc2fc371361a0af
Hemal <hemal.ai.codes@gmail.com>
2025-01-14 16:33:37 +0530

    redirect homepage to swagger documentation

--- transformation_project/transformation_project/urls.py
+++ transformation_project/transformation_project/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
+from django.views.generic import RedirectView
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,4 +38,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', RedirectView.as_view(url='/swagger/', permanent=True)),
 ]
